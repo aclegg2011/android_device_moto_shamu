feebaae9e5fc04241252e34919d7d4a6e1a54253
Aren Clegg <aclegg2011@gmail.com>
2020-04-01 22:48:12 +0000

    shamu: blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,10 +16,10 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/lineage_shamu.mk
+    $(LOCAL_DIR)/bliss_shamu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_shamu-user \
-    lineage_shamu-userdebug \
-    lineage_shamu-eng
+    bliss_shamu-user \
+    bliss_shamu-userdebug \
+    bliss_shamu-eng 
 
